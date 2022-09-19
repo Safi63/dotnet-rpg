6c3edfe2a852d4c8e03d601cf8083d9cf33194b2
Safi Ullah Muhammad <safifiverr786@gmail.com>
2022-09-19 16:17:12 +0500

    Added Character class and Character Controller

---
+++ Models/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dotnet_rpg.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Frodo";
+
+        public int HitPoints { get; set; } = 100;
+
+        public int Strength { get; set; } = 10;
+
+        public int Defense { get; set; } = 10;
+
+        public int Intelligence { get; set; } = 10; 
+
+        public RpgClass Class { get; set; } = RpgClass.Knigt;
+
+        public static implicit operator List<object>(Character v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Models/RpgClass.cs
@@ -0,0 +1,11 @@
+﻿namespace dotnet_rpg.Models
+{
+    public enum RpgClass
+    {
+        Knigt = 1, 
+
+        Mage = 2,
+
+        Cleric = 3,
+    }
+}
